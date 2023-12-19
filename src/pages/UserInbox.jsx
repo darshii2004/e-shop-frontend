07b781775ef4f935fc6a7eb50fc567b328208495
darshii2004 <talaviyadarshita@gmail.com>
2023-12-19 22:54:45 +0530

    basic

--- src/pages/UserInbox.jsx
+++ src/pages/UserInbox.jsx
@@ -9,7 +9,7 @@ import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowRight, AiOutlineSend } from "react-icons/ai";
 import { TfiGallery } from "react-icons/tfi";
 import styles from "../styles/styles";
-const ENDPOINT = "http://localhost:4000/";
+const ENDPOINT = "https://e-shop-socket-darshii2004s-projects.vercel.app/";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const UserInbox = () => {
